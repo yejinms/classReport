0d9dff15d283be15279c575289b77ac07071d394
Yejin Kim <62016534+yejinms@users.noreply.github.com>
2025-01-26 17:46:39 +0000

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,30 @@
 module.exports = {
     root: true,
-    env: { 
+    env: {
       browser: true,
-      es2021: true 
+      es2020: true
     },
     extends: [
       'eslint:recommended',
-      'plugin:react/recommended'
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
     ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { 
+      ecmaVersion: 'latest', 
+      sourceType: 'module' 
     },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  };
+    settings: { 
+      react: { 
+        version: '18.2' 
+      } 
+    },
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
